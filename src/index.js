c0de7c98bebc1a94aa60770a278c1e8eb2ec59a8
Yauheni Safronau <ysafronau26@gmail.com>
2022-07-10 22:49:37 +0300

    feat: complete task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const brackets = bracketsConfig.join('').replace(/,/g, '')
+    const stack = [];
+    if (str.length % 2 === 1) return false;
+    for (let i = 0; i < str.length; i++) {
+        const bracketIndex = brackets.indexOf(str[i]);
+        if (bracketIndex % 2 === 0 && brackets[bracketIndex] === brackets[bracketIndex + 1] && stack[stack.length - 1] === brackets[bracketIndex]) {
+            stack.pop();
+        } else if (bracketIndex % 2 === 0) {
+            stack.push(str[i]);
+        }
+        if ( bracketIndex % 2 === 1 && stack[stack.length - 1] === brackets[bracketIndex - 1]) stack.pop();
+    }
+    return stack.length === 0;
+};
